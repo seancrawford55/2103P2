4265ab6acdbf7991b1aa4ea98c41de0530eeaf52
Sean Crawford <seancrawford55@gmail.com>
2024-11-17 16:34:38 -0600

    Added code in project file

--- CrawfordLinkedListProject.cpp
+++ CrawfordLinkedListProject.cpp
@@ -1,9 +1,41 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+//Using a template allows us to add 
+//any data type we need to the list
+template <typename T>
+struct Node{ 
+    T data;
+    Node* next;
+
+    //Initializes the node with data type
+    Node(T val) : data(val), next(nullptr) {}
+};
+
+//Class for linked list 
+template <typename T>
+class LinkedList{
+    private:
+    Node<T>* head; //Head of list is private to keep it safe
+
+    public:
+    LinkedList() : head(nullptr) {} //Initialize the head of linked list
+    
+    //Deletes entire list
+    ~LinkedList() {
+        Node<T>* current = head;
+        while (current != nullptr) {
+            Node<T>* temp = current;
+            current = current ->next;
+            delete temp;
+        }
+    }
+
 
     
+}
+
+int main(){
 
     return 0;
 
