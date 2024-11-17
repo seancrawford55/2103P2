815249a6030482dddda9079974918cf94b78be09
Sean Crawford <seancrawford55@gmail.com>
2024-11-17 16:11:01 -0600

    Added driver file

---
+++ Project2Driver.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    //Create linked list
+
+    //Call add element
+
+    //Call remove element
+
+    //Call display
+
+    return 0;
+}
